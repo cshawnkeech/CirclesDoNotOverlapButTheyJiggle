e34223c0fd7c331762b0baedaafff1f06b5ccd6c
cshawnkeech <cshawnkeech@gmail.com>
2016-06-28 18:23:22 -0500

    circles now bubble to the top
    changes made in atom & p5js editor, learning git/github

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CirclesDoNotOverlapButTheyJiggle
-A successful implementation of (p5.js Tutorial: 9.5 Random circles with no overlap), a Daniel Shiffman Tutorial.  I've added jiggling behavior with a entry-level function to keep circles from overlapping
+A successful implementation of (p5.js Tutorial: 9.5 Random circles with no overlap),
+a Daniel Shiffman Tutorial.  
+
+Added:
+Jiggling behavior with a entry-level function to keep circles from overlapping
+Changed the variables to favor floating upwards
+Text identifies index of array & round of current x, y location
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var Circle;
 var arrayOfCircles = [];
-var totalCircles = 200;
-framerate(60);
+var totalCircles = 100;
+framerate();
 
 function setup() {
   createCanvas(600, 600);
@@ -11,13 +11,13 @@ function checkOverlap() {
   var overlapping = false;
   for (var i = 0; i < arrayOfCircles.length; i++) {
       var possibleOverlap = dist(proposedCircle.x, proposedCircle.y, arrayOfCircles[i].x, arrayOfCircles[i].y)
-      
+
       if (possibleOverlap < proposedCircle.radius + arrayOfCircles[i].radius) {
         overlapping = true;
         break;
       }
     }
-    
+
     if (!overlapping) {
       return false;
     }
@@ -27,27 +27,28 @@ function checkOverlap() {
 while(arrayOfCircles.length < totalCircles) {
     var proposedCircle = new Circle();
     var safetyValve;
-    
+
     checkOverlap();
     if (checkOverlap() === false) {
       arrayOfCircles.push(proposedCircle);
     }
-    
-    safetyValve++ 
+
+    safetyValve++
     if (safetyValve > 10000) {
+      //totalCircles = arrayOfCircles.length;
       break;
     }
-  } 
-  
+  }
+
 }
- 
-//bumble works fine but does not check for collision 
+
+//bumble works fine and now checks for collision
 function bumble(proposedCircle) {
-    
+
     var mulliganX = proposedCircle.x;
     var mulliganY = proposedCircle.y;
-  
-    var choice = floor(random(4));
+
+    var choice = floor(random(96));
     if (choice === 0) {
       proposedCircle.x += 5/proposedCircle.radius ;
     } else if (choice === 1) {
@@ -56,8 +57,8 @@ function bumble(proposedCircle) {
       proposedCircle.y += 5/proposedCircle.radius;
     } else {
       proposedCircle.y -= 5/proposedCircle.radius ;
-    } 
-    
+    }
+
     //ensure center is on canvas
     //returns to edge if out of bounds
     if (proposedCircle.x > width) {
@@ -68,8 +69,8 @@ function bumble(proposedCircle) {
       proposedCircle.y = height;
     } else if (proposedCircle.y < 0){
       proposedCircle.y = 0;
-    } 
-    
+    }
+
     if (bump(proposedCircle)===true) {
       proposedCircle.x = mulliganX;
       proposedCircle.y = mulliganY;
@@ -78,7 +79,7 @@ function bumble(proposedCircle) {
 
 //bump should check for collision, but I can't get my head around it
 function bump(currentCircleIndex) {
-  
+
   //labels each circle with arrayOfCircles index
   push();
   strokeWeight(1);
@@ -90,41 +91,39 @@ function bump(currentCircleIndex) {
     push();
     textSize(currentCircleIndex.radius * 0.3);
     text(round(currentCircleIndex.x) + ", " + round(currentCircleIndex.y), round(currentCircleIndex.x), round(currentCircleIndex.y) + currentCircleIndex.radius/3);
-    
+
     pop();
   }
-  
+
   pop();
-  
-  
-  
+
+
+
   for (var m = 0; m < arrayOfCircles.length; m++ ) {
     if (m != arrayOfCircles.indexOf(currentCircleIndex)) {
       possibleOverlap = dist(currentCircleIndex.x, currentCircleIndex.y, arrayOfCircles[m].x, arrayOfCircles[m].y);
-      
+
       if (possibleOverlap < currentCircleIndex.radius + arrayOfCircles[m].radius) {
         return true;
       }
     }
   }
-  
+
   return false;
 
-  
+
 }
 
 function draw() {
   background(255);
-  
-  text(frameCount, 20,20);
-  
+
+  stroke(0);
+  //text(frameCount, 20,20);
+
   for (var k = 0; k < arrayOfCircles.length; k++) {
     bumble(arrayOfCircles[k]);
     arrayOfCircles[k].display();
   }
-  
-  
+
+
 }
-
-
-
